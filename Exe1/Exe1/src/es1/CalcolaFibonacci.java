8033675a0959c8e470fc8f03ecb1f400628a78a5
Mazzupa <patrick.mazzu@gmail.com>
2018-04-10 15:32:44 +0200

    Ottimizzato Fibonacci

--- Exe1/Exe1/src/es1/CalcolaFibonacci.java
+++ Exe1/Exe1/src/es1/CalcolaFibonacci.java
@@ -1,9 +1,21 @@
+/**
+ * @author Mazzucchetti Patrick
+ * @matricola 1053212
+ */
 package es1;
 
-public class CalcolaFibonacci{
+/**
+ * Classe contente il Main() per testare la classe Fibonacci
+ * 
+ * ATTENZIONE: MAX Numero Calcolabile = 6998 (Per evitare StakOverflow)
+ */
+public class CalcolaFibonacci {
 	public static void main(String[] args) {
+
+		System.out.println(Fibonacci.get(6998));
+		System.out.println(Fibonacci._get(6998));
 		
-		System.out.println(Fibonacci._get(5));
-		
+		//Fibonacci.stampaSequenza(7);
+
 	}
 }
--- Exe1/Exe1/src/es1/Fibonacci.java
+++ Exe1/Exe1/src/es1/Fibonacci.java
@@ -1,74 +1,166 @@
+/**
+ * @author Mazzucchetti Patrick
+ * @matricola 1053212
+ */
 package es1;
 
+/**
+ * BigInteger per la gestione dei grandi numeri di Fibonacci
+ * HashMap come cache
+ */
+import java.math.BigInteger;
 import java.util.HashMap;
 
+/**
+ * Classe che permette il calcolo dell'n-esimo numero di Fibonacci e la stampa
+ * della sequenza fino all'n-esimo numero di Fibonacci In maniera classica
+ * ricorsiva e multiThreading
+ */
 public class Fibonacci {
+	/**
+	 * Cache introdotta per diminuire il numero di chiamate ricorsive identiche
+	 * (Programmazione dinamica)
+	 */
+	private static HashMap<Integer, BigInteger> cache = new HashMap<>();
 
-	private static HashMap<Integer, Double> cache = new HashMap<>();
-
+	/**
+	 * Stampa a video la sequenza fino all'n-esimo numero di Fibonacci (versione
+	 * ricorsiva classica)
+	 * 
+	 * @param n
+	 *            L'ultimo numero di Fibonacci da stampare
+	 */
 	public static void stampaSequenza(int n) {
 		for (int i = 1; i <= n; i++)
-			System.out.println(get(i));
+			System.out.print(get(i) + " ");
 	}
 
+	/**
+	 * Stampa a video la sequenza fino all'n-esimo numero di Fibonacci (versione
+	 * multiThreading)
+	 * 
+	 * @param n
+	 *            L'ultimo numero di Fibonacci da stampare
+	 */
 	public static void _stampaSequenza(int n) {
 		for (int i = 1; i <= n; i++)
-			System.out.println(_get(i));
+			System.out.print(_get(i) + " ");
 	}
 
-	public static Double get(int n) {
+	/**
+	 * Versione ricorsiva classica
+	 * 
+	 * @param n
+	 *            Numero da calcolare
+	 * @return n-esimo numero
+	 */
+	public static BigInteger get(int n) {
 		if (n == 1)
-			return 0.0;
+			return BigInteger.ZERO;
 		if (n == 2)
-			return 1.0;
+			return BigInteger.ONE;
 		else {
-			if (cache.containsKey(n))
+			if (cache.containsKey(n)) // Uso della programmazione dinamica (Se il numero è già stato calcolato, è
+				// inutle calcolarlo di nuovo)
 				return cache.get(n);
 			else {
-				Double num = get(n - 1) + get(n - 2);
-				cache.put(n, num);
+
+				BigInteger n1 = get(n - 1);
+				BigInteger n2 = get(n - 2);
+				BigInteger num = n1.add(n2);
+
+				cache.put(n, num); // Inserisco il numero calcolato nella cache per non ricalcolarlo di nuovo
 				return num;
 			}
 
 		}
 	}
 
-	public static Double _get(int n) {
+	/**
+	 * Versione multiThreading
+	 * 
+	 * @param n
+	 *            Numero da calcolare
+	 * @return n-esimo numero
+	 */
+	public static BigInteger _get(int n) {
 		if (n == 1)
-			return 0.0;
+			return BigInteger.ZERO;
 		if (n == 2)
-			return 1.0;
+			return BigInteger.ONE;
 
-		MyThread t1 = new MyThread(n - 1);
-		MyThread t2 = new MyThread(n - 2);
+		if (cache.containsKey(n))
+			return cache.get(n);
+		else {
 
-		t1.start();
-		t2.start();
+			// Creazione dei due thread per calcolare l'n-1 esimo e l'n-2 esimo di Fibonacci
+			MyThread t1 = new MyThread(n - 1);
+			MyThread t2 = new MyThread(n - 2);
 
-		try {
-			t1.join();
-			t2.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+			try {
+				// Spawning dei Thread
+				t1.start();
+				t2.start();
+				/**
+				 * Per evitare eccezioni di OutOfMemoryError per via dei troppi thread creati Se
+				 * il numero di Thread spawnati supera il limite massimo consentito dal sistema
+				 * operativo passa alla versione ricorsiva classica
+				 */
+			} catch (java.lang.OutOfMemoryError e) {
+				return get(n);
+			}
+
+			try {
+				// Sync dei Thread per calcolare il risultato finale
+				t1.join();
+				t2.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			BigInteger risultato = t1.getRisultato().add(t2.getRisultato());
+			cache.put(n, risultato);
+			return risultato;
 		}
-
-		return t1.risultato + t2.risultato;
 	}
 
 }
 
+/**
+ * Classe per gestire i Thread per il calcolo della sequenza di Fibonacci
+ */
 class MyThread extends Thread {
-
-	int n;
-	double risultato;
-
+	/**
+	 * Numero di Fibonacci da calcolare
+	 */
+	private int n;
+	/**
+	 * n-esimo numero di Fibonacci calcolato
+	 */
+	private BigInteger risultato;
+	/**
+	 * Costruttore classe
+	 * 
+	 * @param _n
+	 *            Numero di Fibonacci da calcolare
+	 */
 	public MyThread(int _n) {
+		this.risultato = new BigInteger("0");
 		this.n = _n;
 	}
 
+	/**
+	 * Override della funzione run() della classe Thread Assegna a risultato
+	 * l'n-esimo numero di Fibonacci calcolato
+	 */
 	@Override
 	public void run() {
 		risultato = Fibonacci._get(n);
+	}
 
+	/**
+	 * @return n-esimo numero calcolato di Fibonacci
+	 */
+	public BigInteger getRisultato() {
+		return this.risultato;
 	}
 }
